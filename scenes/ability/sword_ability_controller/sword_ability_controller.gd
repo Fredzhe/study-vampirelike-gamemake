b08c97f80e3133f1b4f0928feea0b35021238e1f
Fredzhe <3150118125@qq.com>
2024-09-04 21:38:10 +0800

    v0.0.19版本

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -3,6 +3,8 @@ extends Node
 const MAX_RANGE = 150
 
 @export var sword_ability: PackedScene
+var damage = 5
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,6 +32,8 @@ func on_timer_timeout():
 
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
+	sword_instance.hitbox_component.damage = damage
+	
 	sword_instance.global_position = enemise[0].global_position
 	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0,TAU)) * 4 
 	
--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -4,12 +4,7 @@ const MAX_SPEED = 40
 
 @onready var health_component: HealthComponent = $HealthComponent
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Area2D.area_entered.connect(on_area_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
@@ -23,5 +18,3 @@ func get_direction_to_player():
 	return Vector2.ZERO
 
 
-func on_area_entered(other_area: Area2D):
-	health_component.damage(100)
