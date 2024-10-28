d18b08b52418b25b8ea7f305082dc981ec572bad
Fredzhe <3150118125@qq.com>
2024-10-29 01:05:15 +0800

    v0.0.25

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -19,6 +19,7 @@ func on_level_up(current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
@@ -30,5 +31,7 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
-		
 	print(current_upgrades)
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
