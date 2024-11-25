ab580c0cc92d32cdefbe87828e64ae31d994e234
Fredzhe <3150118125@qq.com>
2024-11-25 17:59:49 +0800

    v0.0.33

---
+++ scenes/ability/axe_ability/axe_ability.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const MAX_RADIOUS = 100
+
+@onready var hitbox_component = $HitboxComponent
+
+# var base_rotation = Vector2.RIGHT #让斧头初始角度随机
+
+func _ready():
+	#base_rotation = Vector2.RIGHT.rotated(randf_range(0, TAU)) #让斧头初始角度随机
+	
+	var tween = create_tween()
+	tween.tween_method(tween_method, 0.0, 2.0, 3)
+	tween.tween_callback(queue_free)
+
+func tween_method(rotations: float):
+	var percent = rotations / 2
+	var current_radius = (percent)* MAX_RADIOUS
+	var current_direction = Vector2.RIGHT.rotated(rotations * TAU)
+	#var current_direction = base_rotation.rotated(rotations * TAU) #让斧头初始角度随机
+	
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+	
+	global_position = player.global_position + (current_direction * current_radius)
