6fc788400541ee55dcdbbf1be51098ca38fafbdd
Fredzhe <3150118125@qq.com>
2024-12-11 16:02:31 +0800

    v0.0.46

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -3,7 +3,8 @@ extends Node
 const MAX_RANGE = 150
 
 @export var sword_ability: PackedScene
-var damage = 5
+var base_damage = 5
+var additional_damage_percent = 1
 var base_wait_time
 
 # Called when the node enters the scene tree for the first time.
@@ -34,7 +35,7 @@ func on_timer_timeout():
 	var sword_instance = sword_ability.instantiate() as Node2D
 	var foreground_layer = get_tree().get_first_node_in_group("foreground_layer")
 	foreground_layer.add_child(sword_instance)
-	sword_instance.hitbox_component.damage = damage
+	sword_instance.hitbox_component.damage = base_damage * additional_damage_percent
 	
 	sword_instance.global_position = enemise[0].global_position
 	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0,TAU)) * 4 
@@ -44,10 +45,10 @@ func on_timer_timeout():
 
 
 func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
-	if upgrade.id != "sword_rate":
-		return
-
-	var percent_reduction = current_upgrades["sword_rate"]["quantity"]* .1
-	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
-	$Timer.start()
-	print($Timer.wait_time)
+	if upgrade.id == "sword_rate":
+		var percent_reduction = current_upgrades["sword_rate"]["quantity"]* .1
+		$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+		$Timer.start()
+		print($Timer.wait_time)
+	elif upgrade.id == "sword_damage":
+		additional_damage_percent = 1 + (current_upgrades["sword_damage"]["quantity"] * .15)
