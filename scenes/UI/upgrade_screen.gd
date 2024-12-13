e67f65488b6bcffe237413e663f4fedf3056058c
Fredzhe <3150118125@qq.com>
2024-12-13 17:47:07 +0800

    v0.0.54

--- scenes/UI/upgrade_screen.gd
+++ scenes/UI/upgrade_screen.gd
@@ -12,15 +12,18 @@ func _ready():
 	
 	
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
-		
+		delay += .08
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)
+	$AnimationPlayer.play("out")
 	get_tree().paused = false
 	queue_free()
 	
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 125
-const ACCELERATION_SMOOTHING = 25
-
 @onready var damage_interal_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
 @onready var abilities = $Abilities
 @onready var animation_player = $AnimationPlayer
 @onready var visuals = $Visuals
+@onready var velocity_component = $VelocityComponent
+
+
 
 var number_colliding_bodies = 0
 var player_face = 1
+var base_speed = 0
 
 func _ready():
+	base_speed = velocity_component.max_speed
+	
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interal_timer.timeout.connect(on_damage_interval_timer_timeout)
@@ -26,9 +29,8 @@ func _ready():
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
-	var target_velocity = direction * MAX_SPEED
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	move_and_slide()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
 
 	if movement_vector.x !=0 || movement_vector.y != 0:
 		animation_player.play("walk")
@@ -78,8 +80,8 @@ func on_health_changed():
 	update_health_display()
 
 func on_ability_upgrade_added(ability_upgrade:AbilityUpgrade, current_upgrades:Dictionary):
-	if not ability_upgrade is Ability:
-		return
-	
-	var ability = ability_upgrade as Ability
-	abilities.add_child(ability_upgrade.ability_controller_scene.instantiate())
+	if ability_upgrade is Ability:
+		var ability = ability_upgrade as Ability
+		abilities.add_child(ability_upgrade.ability_controller_scene.instantiate())
+	elif ability_upgrade.id == "player_speed":
+		velocity_component.max_speed = base_speed + (base_speed * current_upgrades["player_speed"]["quantity"] * .1)
